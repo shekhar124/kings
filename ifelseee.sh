35546099963ab2a477e093387bf156fffa77e9dc
rajashekhar <rajashekharpasikanti@gmail.com>
2021-08-06 16:51:45 +0530

    committed

---
+++ ifelseee.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+var1=40
+var2=50
+if [ $var1 -gt $var2 ]
+
+	then
+	echo "$var2 is greater than $var1"
+
+       	elif [ $var1 > $var2 ]
+
+       	then
+        echo "varable 1 greater"
+
+        else
+	echo "$var2 is less than $var1"
+              
+   fi
+
+
+
